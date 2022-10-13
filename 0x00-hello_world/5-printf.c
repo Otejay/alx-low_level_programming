f699166024a0b75119be52f7920c88abb3f82acd
Otejay <owoadetoyin@gmail.com>
2022-10-13 13:45:53 -0700

    Next step in C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ ** main - serves as the entry point for the program
+ **
+ ** Description: using the main function this program print a statement
+ **
+ ** Return: zero if no error and non zero if error occur
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
