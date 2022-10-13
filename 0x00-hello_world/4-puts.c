34904562048d0188ea678a000304dd81043b1268
Otejay <owoadetoyin@gmail.com>
2022-10-13 13:20:28 -0700

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+** main - Serves as the entry point for the program
+**
+** Description: using the main function this program puts a statement
+**
+** Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
