62d1637e4389a12449d6b6bce95e622adf548fec
Otejay <owoadetoyin@gmail.com>
2022-10-20 13:44:36 -0700

    is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a number
+ * @c: input number
+ *
+ * Return: 1 if it is a number, 0 in other case
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
